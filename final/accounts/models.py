2d476525c455733b450d5cc7bdfe249da14c94d4
Seo DongHoon <gns9541gns@naver.com>
2023-05-24 14:09:10 +0900

    로그인 프로필

--- final/accounts/models.py
+++ final/accounts/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
+# from .models import Movie
 
 # Create your models here.
 class User(AbstractUser):
     pass
+
+
+# User = get_user_model()
+
+# class UserFavoriteMovie(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorite_movies')
+#     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='favorited_by')
+
+#     def __str__(self):
+#         return f'{self.user.username} - {self.movie.title}'
--- final/accounts/views.py
+++ final/accounts/views.py
@@ -1,6 +1,4 @@
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# from django.contrib.auth import get_user_model
+
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
@@ -8,17 +6,18 @@ from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
 
 @api_view(['GET'])
-# @authentication_classes([TokenAuthentication])
-# @permission_classes([IsAuthenticated])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def profile(request):
     user = request.user
     profile_data = {
         'username': user.username,
-        # 'token': request.auth.token
+        'id': user.id,
         # 'email': user.email,
         # 'profile_picture': user.profile_picture.url if user.profile_picture else None,
     }
     return Response(profile_data)
+
 
 # @api_view(['GET'])
 # def profile(request):
